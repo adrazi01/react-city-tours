1e744607d783d4df26662c815e857fcb5e5c69a1
adrazi01 <adrazi01@fesb.hr>
2021-09-29 12:11:32 +0200

    Last commit

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -2,36 +2,40 @@ import React, { Component } from "react";
 import "./Tour.scss"
 
 export default class Tour extends Component {
+  state = {
+    showInfo: false
+  }
+  handleInfo = () => {
+    this.setState({
+      showInfo:!this.state.showInfo
+    })
+  }
   render() {
-    const{}
+    const{id, city, img, name, info} = this.props.tour
+    const {removeTour} = this.props;
     return (
       <article className="tour">
         <div className="img-container">
           <img
-            src="https://images.pexels.com/photos/6913782/pexels-photo-6913782.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          src={img}
             alt=""
           />
-          <span className="close-btn">
+          <span className="close-btn" onClick={() => {removeTour(id)}}>
             <i className="fas fa-window-close" />
           </span>
         </div>
         <div className="tour-info">
-          <h3>city</h3>
-          <h4>name</h4>
+          <h3>{city}</h3>
+          <h4>{name}</h4>
           <h5>
             info{" "}
-            <span>
+            <span onClick={this.handleInfo}>
               <i className="fas fa-caret-square-down" />
             </span>
           </h5>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod
-            sem nisl, ut imperdiet leo imperdiet suscipit. Etiam viverra elit
-            nisi, a vehicula augue imperdiet nec. Vivamus quis dapibus felis. Ut
-            vehicula finibus magna, sit amet lobortis odio placerat et. Cras
-            interdum nec nunc ac tristique. Nullam tincidunt tellus sed erat
-            pretium, quis aliquam nulla auctor.
-          </p>
+          {this.state.showInfo && <p>
+            {info}
+          </p>}
         </div>
       </article>
     );
